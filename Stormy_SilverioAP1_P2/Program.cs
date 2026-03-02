a2097c094af738e47db98b024ac7bfae376f36e3
StormyUCNE <stormy_silverio@ucne.edu.do>
2026-03-02 19:06:08 -0400

    Inyecta el contexto.

--- Stormy_SilverioAP1_P2/Program.cs
+++ Stormy_SilverioAP1_P2/Program.cs
@@ -1,5 +1,7 @@
 using Blazored.Toast;
+using Microsoft.EntityFrameworkCore;
 using Stormy_SilverioAP1_P2.Components;
+using Stormy_SilverioAP1_P2.DAL;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,7 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 var ConStr = builder.Configuration.GetConnectionString("SqlConStr");
+builder.Services.AddDbContextFactory<Contexto>(o => o.UseSqlServer(ConStr))
 
 builder.Services.AddBlazoredToast();
 var app = builder.Build();
