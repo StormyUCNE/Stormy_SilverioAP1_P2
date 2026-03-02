7640051ef4707eff5730c0840952141a245c382a
StormyUCNE <stormy_silverio@ucne.edu.do>
2026-03-02 19:04:28 -0400

    Crea el contexto.

---
+++ Stormy_SilverioAP1_P2/DAL/Contexto.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Stormy_SilverioAP1_P2.Models;
+
+namespace Stormy_SilverioAP1_P2.DAL;
+public class Contexto: DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options): base(options) { }
+    public DbSet<ViajesEspaciales> ViajesEspaciales { get; set; }
+}
+
