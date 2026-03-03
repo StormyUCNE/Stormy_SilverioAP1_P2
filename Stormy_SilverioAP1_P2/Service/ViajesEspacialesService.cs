ac8630b2a5a23aabb2222bb62ec6e0b1c7bb063d
StormyUCNE <stormy_silverio@ucne.edu.do>
2026-03-02 20:36:33 -0400

    Agrega el Service

---
+++ Stormy_SilverioAP1_P2/Service/ViajesEspacialesService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Stormy_SilverioAP1_P2.DAL;
+using Stormy_SilverioAP1_P2.Models;
+using System.Linq.Expressions;
+namespace Stormy_SilverioAP1_P2.Service;
+
+public class ViajesEspacialesService(IDbContextFactory<Contexto> DbFactory)
+{
+    public async Task<bool> Guardar()
+    {
+        return false;
+    }
+    public async Task<bool> Exise()
+    {
+        return false;
+    }
+    public async Task<bool> Insertar()
+    {
+        return false;
+    }
+    public async Task<bool> Modificar()
+    {
+        return false;
+    }
+    public async Task<bool> Buscar()
+    {
+        return false;
+    }
+    public async Task<bool> Eliminar()
+    {
+        return false;
+    }
+    public async Task<List<ViajesEspaciales>> Listar(Expression<Func<ViajesEspaciales, bool>> criterio)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.ViajesEspaciales.Where(criterio).AsNoTracking().ToListAsync();
+    }
+}
